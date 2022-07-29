c3c350b7a7241f4225d643e9d84b18d3dda924dd
mdmegha1999 <94183187+mdmegha1999@users.noreply.github.com>
2022-07-30 01:21:21 +0530

    Update AddStore.js

--- src/components/AddStore.js
+++ src/components/AddStore.js
@@ -2,7 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
-
+impore './Btn.css';
 const useStyles = makeStyles((theme) => ({
   root: {
     "& > *": {
