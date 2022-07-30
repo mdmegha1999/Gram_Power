fcb8305d9e7ebbdd0a8f5c41657437b555a40357
mdmegha1999 <94183187+mdmegha1999@users.noreply.github.com>
2022-07-30 23:34:16 +0530

    Update ViewStore.js

--- src/components/ViewStore.js
+++ src/components/ViewStore.js
@@ -1,57 +1,61 @@
 import React from "react";
-
+import './Style.css';
 function ViewStore(props) {
   return (
-    <div className="hours_day">
-      <h1>{props.storeDetails.title}</h1>
+    <>
+    <div id="store_content">
+      <h1 className="heading">{props.storeDetails.title}</h1>
       <div className="store_details_container">
         <img
           className="cover_img"
           src={props.storeDetails.cover}
           alt="cover-img"
         />
+        
         <div className="photo_gallery">
           <img src={props.storeDetails.galleryImg1} alt="img1" />
           <img src={props.storeDetails.galleryImg2} alt="img2" />
           <img src={props.storeDetails.galleryImg3} alt="img3" />
         </div>
+        
         <div className="store_hours">
-          <h4>Shope Hours</h4>
+          <h3>Shope Hours</h3>
           <p>
-            Monday: {props.storeDetails.storeHrs.from} -{" "}
+            <b>Monday:</b> {props.storeDetails.storeHrs.from} -{" "}
             {props.storeDetails.storeHrs.to}
           </p>
           <p>
-            Tuesday: {props.storeDetails.storeHrs.from} -{" "}
+            <b>Tuesday:</b> {props.storeDetails.storeHrs.from} -{" "}
             {props.storeDetails.storeHrs.to}
           </p>
           <p>
-            Wednesday: {props.storeDetails.storeHrs.from} -{" "}
+            <b>Wednesday:</b> {props.storeDetails.storeHrs.from} -{" "}
             {props.storeDetails.storeHrs.to}
           </p>
           <p>
-            Thursday: {props.storeDetails.storeHrs.from} -{" "}
+            <b>Thursday:</b> {props.storeDetails.storeHrs.from} -{" "}
             {props.storeDetails.storeHrs.to}
           </p>
           <p>
-            Friday: {props.storeDetails.storeHrs.from} -{" "}
+           <b> Friday:</b> {props.storeDetails.storeHrs.from} -{" "}
             {props.storeDetails.storeHrs.to}
           </p>
           <p>
-            Saturday: {props.storeDetails.storeHrs.from} -{" "}
+            <b>Saturday:</b> {props.storeDetails.storeHrs.from} -{" "}
             {props.storeDetails.storeHrs.to}
           </p>
           <p>
-            Sunday: {props.storeDetails.storeHrs.from} -{" "}
+            <b>Sunday:</b> {props.storeDetails.storeHrs.from} -{" "}
             {props.storeDetails.storeHrs.to}
           </p>
         </div>
         <div className="about_us">
-          <h4>About Us</h4>
+          <h3>About Us</h3>
           <p>{props.storeDetails.about}</p>
         </div>
+        
         <div className="store_location">
-          <h4>Location</h4>
+          <h3>Location</h3>
           <p>{props.storeDetails.location}</p>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d227748.99973450298!2d75.65047228361074!3d26.88514167956319!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396c4adf4c57e281%3A0xce1c63a0cf22e09!2sJaipur%2C%20Rajasthan!5e0!3m2!1sen!2sin!4v1659024492208!5m2!1sen!2sin"
@@ -60,8 +64,9 @@ function ViewStore(props) {
             title="map"
           ></iframe>
         </div>
+        
         <div className="product_range">
-          <h4>Product Range</h4>
+          <h3>Product Range</h3>
           <p>We provide products in the following categories</p>
           <ul>
             <li>{props.storeDetails.categories.category1}</li>
@@ -70,7 +75,8 @@ function ViewStore(props) {
           </ul>
         </div>
       </div>
-    </div>
+      </div>
+    </>
   );
 }
 
